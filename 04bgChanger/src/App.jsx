b32451c83b2f803104705cc2e36e2a704b249011
Shivam Srivastava <shivam0174srivastava@gmail.com>
2024-12-05 16:01:41 +0530

    added a project password generator

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+       <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-2xl {color} px-3 py-4 rounded-3xl'>
             <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"black"}}
